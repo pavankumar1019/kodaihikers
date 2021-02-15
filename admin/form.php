10980bd935d1771f55af98fda879bca41e60eebb
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-15 16:21:58 +0530

    a

--- admin/form.php
+++ admin/form.php
@@ -1535,7 +1535,7 @@ $result = $conn->query($sql);
                 cache: false,
                 contentType: false,
                 processData: false,
-                data:  new FormData(this),,                         
+                data:  new FormData(this),                         
                 type: 'post',
                 success: function(dataResult){
 					var dataResult = JSON.parse(dataResult);
