d56e3d1da44bc50f67330269cb2f6d9234bd7707
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-15 15:27:58 +0530

    a

--- admin/form.php
+++ admin/form.php
@@ -480,12 +480,13 @@ $result = $conn->query($sql);
                                             <h3 class="text-center title-2">Home page Carousels</h3>
                                         </div>
                                         <hr>
-                                        <form action="" method="post" novalidate="novalidate">
+                                        <form action="" method="post" id="my_form" novalidate="novalidate">
                                             <div class="form-group">
                                                <img class="img-fluid" style="width: 300px;" src="../images/<?=$row['path'];?>" alt="">
                                             </div>
                                             <div class="form-group has-success">
                                                 <label for="cc-name" class="control-label mb-1">Choose Image To Change</label>
+                                                <input id="id" type="text" name="id" value="<?=$row['id'];?>" />
                                                 <input id="uploadImage" type="file" accept="image/*" name="image" class="uploadimg"/>
                                                 <span class="help-block field-validation-valid" data-valmsg-for="cc-name" data-valmsg-replace="true"></span>
                                             </div>
@@ -1505,7 +1506,8 @@ $result = $conn->query($sql);
             $('#upload_carousels').on('click', function() {
                 console.log("test");
             var file_data = $('.uploadimg').prop('files')[0];   
-            var form_data = new FormData();
+            var form_data = new FormData('#my_form');
+           
             $(".loder").css("visibility", "visible");
             
             $("#upload_carousels").attr("disabled", "disabled");
