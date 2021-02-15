2e21ef5b165e558237f215e5ee914c705c38f588
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-15 15:46:31 +0530

    a

--- admin/upload_carousels/upload.php
+++ admin/upload_carousels/upload.php
@@ -21,8 +21,7 @@ $path=$_POST["id"];
            
             $result = move_uploaded_file($_FILES['file']['tmp_name'], '../../images/' . $code.'.'.$ext);
             echo json_encode(array("statusCode"=>200 ,'code'=>$code));
-            $sql = "INSERT INTO `kh_dynamic_scrolling_banners`( `path`) 
-	VALUES ('$path')";
+    $sql="UPDATE kh_dynamic_scrolling_banners SET path='$code.$ext' WHERE id=$path"
     mysqli_query($conn, $sql);
         }
         
