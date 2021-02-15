4949389d14f4f6a0dda8efade85d459cb95621ea
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-15 16:47:13 +0530

    a

--- admin/upload_carousels/upload.php
+++ admin/upload_carousels/upload.php
@@ -1,29 +1,30 @@
 <?php
-if(!empty($_FILES['picture']['name'])){
-    //Include database configuration file
-    include_once '../db_connection/db.php';
-    
-    
-    //File uplaod configuration
-    $result = 0;
-    $uploadDir = "../../images/";
-    $fileName = time().'_'.basename($_FILES['picture']['name']);
-    $targetPath = $uploadDir. $fileName;
-    $userId=$_POST['id'];
-    //Upload file to server
-    if(@move_uploaded_file($_FILES['picture']['tmp_name'], $targetPath)){
-        //Get current user ID from session
-        
-        
-        //Update picture name in the database
-        $update = $conn->query("UPDATE kh_dynamic_scrolling_banners SET path = '".$fileName."' WHERE id = $userId");
-        
-        //Update status
-        if($update){
-            $result = 1;
-        }
+include '../../db_connection/db.php';
+$valid_extensions = array('jpeg', 'jpg','png'); 
+$path=$_POST["id"];
+    if ( 0 < $_FILES['file']['error'] ) {
+        echo 'Error: ' . $_FILES['file']['error'] . '<br>';
     }
-    
-    //Load JavaScript function to show the upload status
-    echo '<script type="text/javascript">window.top.window.completeUpload(' . $result . ',\'' . $targetPath . '\');</script>  ';
-}
+    else {
+       
+        $code=mt_rand(10,100000);/* rename the file name*/
+        $size= $_FILES['file']['size'];
+        $ext = strtolower(pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION));
+        if($size > 2097152) /*2 mb 1024*1024 bytes*/
+        {
+            echo json_encode(array("statusCode"=>400,'msg'=>"Image allowd less than 2 mb"));
+        }
+        else if(!in_array($ext, $valid_extensions)) {
+            echo json_encode(array("statusCode"=>400,'msg'=>$ext.' not allowed'));
+        }
+        else{
+           
+            $result = move_uploaded_file($_FILES['file']['tmp_name'], '../../images/' . $code.'.'.$ext);
+            $sql="UPDATE kh_dynamic_scrolling_banners SET path='$code.$ext' WHERE id=$path";
+            mysqli_query($conn, $sql);
+            echo json_encode(array("statusCode"=>200 ,'code'=>$code));
+   
+        }
+        
+    }
+?>
