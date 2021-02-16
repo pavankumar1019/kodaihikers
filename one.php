75b108e7510144c26ab1221b843feddb11ad5d57
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-16 22:14:05 +0530

    a

--- one.php
+++ one.php
@@ -1,10 +1,1 @@
 <h1>Hello</h1>
-<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
-<script>
-  window.OneSignal = window.OneSignal || [];
-  OneSignal.push(function() {
-    OneSignal.init({
-      appId: "678fdb16-8824-4b99-b872-4281424bafbd",
-    });
-  });
-</script>
