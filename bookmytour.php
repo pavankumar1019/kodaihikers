2d3d6bae064e18ddb696e4a8c837f110922b20eb
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-18 02:39:50 +0530

    a

--- bookmytour.php
+++ bookmytour.php
@@ -402,7 +402,7 @@ Technologies
 
 </div>
 
-<script src="js/jquery-3.2.1.min.js"></script>
+
 <script src="styles/bootstrap4/popper.js"></script>
 <script src="styles/bootstrap4/bootstrap.min.js"></script>
 <script src="plugins/colorbox/jquery.colorbox-min.js"></script>
