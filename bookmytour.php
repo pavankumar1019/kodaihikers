d8df23ee003d6445e54eac0c04097620814035c6
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-18 02:50:21 +0530

    a

--- bookmytour.php
+++ bookmytour.php
@@ -401,6 +401,13 @@ Technologies
 	</div>
 
 </div>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="styles/bootstrap4/popper.js"></script>
+<script src="styles/bootstrap4/bootstrap.min.js"></script>
+<script src="plugins/colorbox/jquery.colorbox-min.js"></script>
+<script src="plugins/parallax-js-master/parallax.min.js"></script>
+<script src="js/blog_custom.js"></script>
 <script>
 (document).ready(function () {
     $(".content").hide();
@@ -411,13 +418,6 @@ Technologies
     });
 });
 </script>
-<script src="js/jquery-3.2.1.min.js"></script>
-<script src="styles/bootstrap4/popper.js"></script>
-<script src="styles/bootstrap4/bootstrap.min.js"></script>
-<script src="plugins/colorbox/jquery.colorbox-min.js"></script>
-<script src="plugins/parallax-js-master/parallax.min.js"></script>
-<script src="js/blog_custom.js"></script>
-
 </body>
 
 </html>
