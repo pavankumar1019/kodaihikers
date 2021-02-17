862c09f2b649148b2f50c97029fc46a8cf87fd07
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-18 02:30:52 +0530

    a

--- bookmytour.php
+++ bookmytour.php
@@ -414,7 +414,7 @@ $(document).ready(function () {
     $(".show_hide").on("click", function () {
         var txt = $(".content").is(':visible') ? 'Read More' : 'Read Less';
         $(".show_hide").text(txt);
-        $(this).next('.content').slideToggle(200);
+        $(this).next('.content').slideToggle(800);
     });
 });
 </script>
