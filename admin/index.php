436e74c37aca4c96392f9b014c21b778a506f71d
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-18 03:04:37 +0530

    a

--- admin/index.php
+++ admin/index.php
@@ -455,8 +455,8 @@ if(!isset($_SESSION['email']) || empty($_SESSION['name'])) {
                             <div class="col-md-12">
                                 <div class="overview-wrap">
                                     <h2 class="title-1">overview</h2>
-                                    <button class="au-btn au-btn-icon au-btn--blue">
-                                        <i class="zmdi zmdi-plus"></i>add item</button>
+                                    <!-- <button class="au-btn au-btn-icon au-btn--blue">
+                                        <i class="zmdi zmdi-plus"></i>add item</button> -->
                                 </div>
                             </div>
                         </div>
