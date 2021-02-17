882b0161ebc91c0a507adca91f08e7dbdbf60380
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-18 00:18:04 +0530

    a

--- admin/login.php
+++ admin/login.php
@@ -30,31 +30,6 @@ include '../db_connection/db.php';
 
 </head>
 <body>
-<div class="container-fluid">
-<div class="container d-flex p-2">
-                   
-                            <form name="frmUser" method="post" action="" >
-                            <div class="mb-3">
-                              <label for="exampleInputEmail1" class="form-label">Email address</label>
-                              <input class="form-control"  id="exampleInputEmail1" aria-describedby="emailHelp" type="text" name="user_name" required>
-                              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                            </div>
-                            <div class="mb-3">
-                              <label for="exampleInputPassword1" class="form-label">Password</label>
-                              <input  type="password" name="password" class="form-control" id="exampleInputPassword1" required>
-                            </div>
-                            <div class="mb-3">
-                            <div class="message"><?php if($message!="") { echo $message; } ?></div>
-
-                            </div>
-                            <div class="d-grid gap-2">
-                            <input type="submit"  class="btn btn-primary" name="submit" value="Submit"><br><br>
-
-                            </div>
-                          </form>
-
-                    </div>
-                    </div>
 
 </body>
 </html>
