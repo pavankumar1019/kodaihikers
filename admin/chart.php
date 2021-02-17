1cc8372dac4e6d3ff36e2f85abe59eed85c4f56b
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-17 22:56:22 +0530

    a

--- admin/chart.php
+++ admin/chart.php
@@ -391,23 +391,23 @@
                                 <div class="account-wrap">
                                     <div class="account-item clearfix js-item-menu">
                                         <div class="image">
-                                            <img src="images/icon/avatar-01.jpg" alt="John Doe" />
+                                            <img src="../images/kodai_logo.png" alt="<?php echo $_SESSION['name'];?>" />
                                         </div>
                                         <div class="content">
-                                            <a class="js-acc-btn" href="#">john doe</a>
+                                            <a class="js-acc-btn" href="#"><?php echo $_SESSION['name'];?></a>
                                         </div>
                                         <div class="account-dropdown js-dropdown">
                                             <div class="info clearfix">
                                                 <div class="image">
                                                     <a href="#">
-                                                        <img src="images/icon/avatar-01.jpg" alt="John Doe" />
+                                                        <img src="../images/kodai_logo.png" alt="<?php echo $_SESSION['name'];?>" />
                                                     </a>
                                                 </div>
                                                 <div class="content">
                                                     <h5 class="name">
-                                                        <a href="#">john doe</a>
+                                                        <a href="#"><?php echo $_SESSION['name'];?></a>
                                                     </h5>
-                                                    <span class="email">johndoe@example.com</span>
+                                                    <span class="email"><?php echo $_SESSION['email'];?></span>
                                                 </div>
                                             </div>
                                             <div class="account-dropdown__body">
