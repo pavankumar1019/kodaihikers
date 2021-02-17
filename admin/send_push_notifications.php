bcda443c03fb73534912932004a58edddf2a2354
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-17 13:11:39 +0530

    a

--- admin/send_push_notifications.php
+++ admin/send_push_notifications.php
@@ -46,60 +46,7 @@ if ($uploadOk == 0) {
   if (move_uploaded_file($_FILES["fileToUpload2"]["tmp_name"], $target_file)) {
       $first_name=$_FILES["fileToUpload2"]["name"];
 
-      function sendMessage() {
-        $content      = array(
-            "en" => $message
-        );
-        $headings      = array(
-            "en" => $title
-        );
-    
-        $fields = array(
-            'app_id' => "678fdb16-8824-4b99-b872-4281424bafbd",
-            'included_segments' => array(
-                'Subscribed Users'
-            ),
-            'contents' => $content,
-            'headings' => $headings,
-            "chrome_web_image" => "https://www.kodaihikers.com/push_notification/"."$first_name",
-            "url" => "https://kodaihikers.com"
-         
-        );
-        
-        $fields = json_encode($fields);
-        print("\nJSON sent:\n");
-        print($fields);
-        
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json; charset=utf-8',
-            'Authorization: Basic MmIyNzMxMzEtZjU3Zi00MjIzLWIyYmQtY2M1ODMxMzc2YTU3'
-        ));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_HEADER, FALSE);
-        curl_setopt($ch, CURLOPT_POST, TRUE);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        
-        $response = curl_exec($ch);
-        curl_close($ch);
-        
-        return $response;
-    }
-    
-    $response = sendMessage();
-    $return["allresponses"] = $response;
-    $return = json_encode($return);
-    
-    $data = json_decode($response, true);
-    print_r($data);
-    $id = $data['id'];
-    print_r($id);
-    
-    print("\n\nJSON received:\n");
-    print($return);
-    print("\n");
+      
 
 
 
