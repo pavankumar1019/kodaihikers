b6c8af398078ec034cc202a1e343bc710b8364c6
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-18 02:24:44 +0530

    a

--- bookmytour.php
+++ bookmytour.php
@@ -20,6 +20,7 @@ $result2 = $conn->query($sql2);
 <link href="plugins/colorbox/colorbox.css" rel="stylesheet" type="text/css">
 <link rel="stylesheet" type="text/css" href="styles/blog_styles.css">
 <link rel="stylesheet" type="text/css" href="styles/blog_responsive.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 <body>
@@ -157,8 +158,8 @@ $result2 = $conn->query($sql2);
 							<div class="blog_post_text">
 								<p>Aenean in lacus ligula. Phasellus euismod gravida eros. Aenean nec ipsum aliquet, pharetra magna id, interdum sapien. Etiam id lorem eu nisl pellentesque semper. Nullam tincidunt metus placerat, suscipit leo ut, tempus nulla. Fusce at eleifend tellus. Ut eleifend dui nunc, non fermentum qua.</p>
 							</div>
-							<div class="blog_post_title"><a href="#">Inclusions</a></div>
-							<div class="blog_post_text">
+							<div class="blog_post_title content"><a href="#">Inclusions</a></div>
+							<div class="blog_post_text content">
 								<p>Accomodation as per Itinerary
 Daily Breakfast
 Important Notes:-
@@ -170,8 +171,8 @@ Suggested activities are not included in the itinerary
 Toll charges and Road taxes are not included in the cost of the package
 If you plan to travel with an infant, then it is advised to carry an infant car seats</p>
 							</div>
-							<div class="blog_post_title"><a href="#">Exclusions</a></div>
-							<div class="blog_post_text">
+							<div class="blog_post_title content"><a href="#">Exclusions</a></div>
+							<div class="blog_post_text content">
 								<p>
 5% GSt Extra
 Anything not mentioned under ‘Package Inclusions’
@@ -182,7 +183,8 @@ Medical and travel insurance
 </p>
 							</div>
 							<br>
-							<div class="button home_slider_button">
+							<a href="" class="show_hide"></a>
+							<div class="button home_slider_button content" >
 									<div class="button_bcg"></div><a href="contact.php">book now
 										us<span></span><span></span><span></span></a>
 								</div>
@@ -403,7 +405,16 @@ Technologies
 <script src="plugins/colorbox/jquery.colorbox-min.js"></script>
 <script src="plugins/parallax-js-master/parallax.min.js"></script>
 <script src="js/blog_custom.js"></script>
-
+<script>
+$(document).ready(function () {
+    $(".content").hide();
+    $(".show_hide").on("click", function () {
+        var txt = $(".content").is(':visible') ? 'Read More' : 'Read Less';
+        $(".show_hide").text(txt);
+        $(this).next('.content').slideToggle(200);
+    });
+});
+</script>
 </body>
 
 </html>
