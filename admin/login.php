b0a36a24e6363edfdb3de1495cddc965404800e2
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-17 23:55:44 +0530

    a

--- admin/login.php
+++ admin/login.php
@@ -20,11 +20,11 @@ include '../db_connection/db.php';
 <html>
 <head>
 
- <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="au theme template">
-    <meta name="author" content="Hau Nguyen">
-    <meta name="keywords" content="au theme template">
+<title>KODI HIKERS</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="Kodai Hikers is a best tours and travels agency in Kodaikanal ">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
     <title>User Login</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
