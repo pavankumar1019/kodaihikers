859fd9a69c645dcaec9ae59aed639d453f8390b5
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-17 20:06:55 +0530

    a

--- save.php
+++ save.php
@@ -1,9 +1,10 @@
 <?php
 	include 'db_connection/db.php';
+    
 	$name=$_POST['name'];
 	$email=$_POST['email'];
 	$phone=$_POST['phone'];
-	$message=$_POST['message'];
+	$city=$_POST['city'];
 	$sql = "INSERT INTO `kh_get_in_touch`( `name`, `email`, `mobile_number`, `message`) 
 	VALUES ('$name','$email','$phone','$message')";
 	if (mysqli_query($conn, $sql)) {
