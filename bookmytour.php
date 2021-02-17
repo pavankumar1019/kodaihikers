28aee24fe71a59cd5587580a890e43aca48e01f7
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-18 02:32:35 +0530

    a

--- bookmytour.php
+++ bookmytour.php
@@ -412,9 +412,8 @@ Technologies
 $(document).ready(function () {
     $(".content").hide();
     $(".show_hide").on("click", function () {
-        var txt = $(".content").is(':visible') ? 'Read More' : 'Read Less';
-        $(".show_hide").text(txt);
-        $(this).next('.content').slideToggle(800);
+		$(".content").show();
+		$(this).hide();
     });
 });
 </script>
