6b696e4e27f2c4a25d349a49653e83254f19f79f
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-17 13:53:00 +0530

    a

--- admin/send_push_notifications.php
+++ admin/send_push_notifications.php
@@ -60,7 +60,7 @@ if ($uploadOk == 0) {
             ),
             'contents' => $content,
             'headings' => $headings,
-            "chrome_web_image" => "https://www.kodaihikers.com/push_notification/"."$first_name",
+            "chrome_web_image" => "https://www.kodaihikers.com/push_notification/".$first_name,
             "url" => "https://kodaihikers.com"
          
         );
