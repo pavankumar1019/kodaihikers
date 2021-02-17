f7df0d6590ca827a3d6b543bfee0f2bdd74103b6
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-17 13:38:05 +0530

    a

--- admin/send_push_notifications.php
+++ admin/send_push_notifications.php
@@ -47,10 +47,10 @@ if ($uploadOk == 0) {
       $first_name=$_FILES["fileToUpload2"]["name"];
       function sendMessage() {
         $content      = array(
-            "en" => "$message"
+            "en" => $message
         );
         $headings      = array(
-            "en" => "$title"
+            "en" => $title
         );
     
         $fields = array(
