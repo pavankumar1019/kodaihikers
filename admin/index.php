200424de5189f41cc0fa8870cc10527d707ae979
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-17 22:28:46 +0530

    a

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,13 @@
+
+<?php
+session_start();
+
+if(!isset($_SESSION['email']) || empty($_SESSION['name'])) {
+   
+} else {
+	header("Location: login.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
