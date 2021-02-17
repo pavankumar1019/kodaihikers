4c8a0b719b99256f96a3db1f0271db2088b03d05
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-18 03:20:24 +0530

    a

--- admin/index.php
+++ admin/index.php
@@ -794,6 +794,11 @@ if(!isset($_SESSION['email']) || empty($_SESSION['name'])) {
                                     </div>
                                 </div>
                             </div>
+
+
+
+                        </div>
+                        <div class="row">
                             <div class="col-lg-6">
                                 <div class="au-card au-card--no-shadow au-card--no-pad m-b-40">
                                     <div class="au-card-title" style="background-image:url('images/bg-title-01.jpg');">
@@ -861,6 +866,7 @@ if(!isset($_SESSION['email']) || empty($_SESSION['name'])) {
                                     </div>
                                 </div>
                             </div>
+
 
 
                         </div>
