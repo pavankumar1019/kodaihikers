9689a2ac3b12023176855c4a33808faf59d9a3ad
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-18 19:54:45 +0530

    a

--- contact.php
+++ contact.php
@@ -177,7 +177,7 @@ $result3 = $conn->query($sql3);
 				<div class="col-lg-6">
 
 					<!-- Contact Form -->
-					<div class="container-fluid bg-primary" style="height:300px">
+					<div class="container-fluid bg-info">
 						<div class="contact_title text-center">Online Payment</div>
 						
 						<div class="container md-6" style="color:white;">
