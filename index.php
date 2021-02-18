a26ae53813a218dee0c7909f1e35ad0e5c4798b4
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-18 22:00:38 +0530

    a

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ $result5 = $conn->query($sql5);
             button_color: "#FF6550", // Color of button
             position: "left", // Position may be 'right' or 'left'
             order: "whatsapp,viber", // Order of buttons
-            pre_filled_message: "hi, how can we help you.", // WhatsApp pre-filled message
+            pre_filled_message: "hi", // WhatsApp pre-filled message
         };
         var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
         var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
