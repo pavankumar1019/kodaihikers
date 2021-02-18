f3de20f76e91c75c6145753aef0cf9d864cc6469
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-18 15:55:17 +0530

    a

--- index.php
+++ index.php
@@ -14,6 +14,9 @@ $result3 = $conn->query($sql3);
 
 $sql4 = "SELECT * FROM kh_book_my_tours ORDER BY date DESC LIMIT 2";
 $result4 = $conn->query($sql4);
+
+$sql5 = "SELECT * FROM kh_blog ORDER BY date DESC LIMIT 3";
+$result5 = $conn->query($sql5);
     ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -1022,37 +1025,35 @@ $result4 = $conn->query($sql4);
 							<div class="footer_content footer_blog">
 
 								<!-- Footer blog item -->
-								<div class="footer_blog_item clearfix">
-									<div class="footer_blog_image"><img src="images/kodai_footerblog1.jpg"
+
+								<?php
+                            if ($result5->num_rows > 0) {
+                                // output data of each row
+                                while($row5 = $result5->fetch_assoc()) {
+                              
+                                                            ?>
+									<li class="social_list_item"><a href="<?php echo $row2["links"]; ?>"><?php echo $row2["social_media_icon"]; ?></a></li>
+									<div class="footer_blog_item clearfix">
+									<div class="footer_blog_image"><img src="./blogimages/<?php echo $row5["image"]; ?>"
 											alt="https://unsplash.com/@avidenov"></div>
 									<div class="footer_blog_content">
 										<div class="footer_blog_title"><a href="blog.php">Beauty of river. </a>
 										</div>
-										<div class="footer_blog_date">Feb 14, 2021</div>
+										<div class="footer_blog_date" style="color:white;"><?php echo $row5["image"]; ?></div>
 									</div>
 								</div>
 
-								<!-- Footer blog item -->
-								<div class="footer_blog_item clearfix">
-									<div class="footer_blog_image"><img src="images/kodai_footerblog2.png"
-											alt="https://unsplash.com/@deannaritchie"></div>
-									<div class="footer_blog_content">
-										<div class="footer_blog_title"><a href="blog.php">Chettiar Park. </a>
-										</div>
-										<div class="footer_blog_date">Feb 14, 2021</div>
-									</div>
-								</div>
+                                                            <?php
+								}
+								}
+								?>
+								
 
 								<!-- Footer blog item -->
-								<div class="footer_blog_item clearfix">
-									<div class="footer_blog_image"><img src="images/kodai_footerblog3.webp"
-											alt="https://unsplash.com/@bergeryap87"></div>
-									<div class="footer_blog_content">
-										<div class="footer_blog_title"><a href="blog.php">Kodaikanal. </a>
-										</div>
-										<div class="footer_blog_date">Feb 14, 2021</div>
-									</div>
-								</div>
+								
+
+								<!-- Footer blog item -->
+								
 
 							</div>
 						</div>
