ebb870d937fdb57954b69d88da4fd90910a2c06b
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-18 23:09:21 +0530

    a

--- index.php
+++ index.php
@@ -66,12 +66,33 @@ $result5 = $conn->query($sql5);
 .my-float{
 	margin-top:16px;
 }
+	.float1{
+	position:fixed;
+	width:60px;
+	height:60px;
+	bottom:80px;
+	right:10px;
+	background-color:#25d366;
+	color:#FFF;
+	border-radius:50px;
+	text-align:center;
+  font-size:30px;
+	box-shadow: 2px 2px 3px #999;
+  z-index:100;
+}
+
+.my-float1{
+	margin-top:16px;
+}
 </style>
 </head>
 
 <body>
 <a href="https://api.whatsapp.com/send?phone=+917483737698&text=Hello" class="float" target="_blank">
 <i class="fa fa-whatsapp my-float"></i>
+</a>
+<a href="https://api.whatsapp.com/send?phone=+917483737698&text=Hello" class="float1" target="_blank">
+<i class="fa fa-whatsapp my-float1"></i>
 </a>
 	<div class="super_container">
 
