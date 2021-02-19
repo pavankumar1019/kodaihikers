d80e6c1caa90352f0305dea650d128432fc090dc
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-19 22:37:53 +0530

    a

--- weather/main.js
+++ weather/main.js
@@ -2,11 +2,23 @@ const api = {
   key: "2b86e74fb5be73f5b02b0298b2b622fa",
   base: "https://api.openweathermap.org/data/2.5/"
 }
-  fetch(`${api.base}weather?q=kodaikanal&units=metric&APPID=${api.key}`)
+window.addEventListener('load', (event) => {
+  const searchbox = document.querySelector('.search-box');
+  searchbox.addEventListener('keypress', setQuery);
+});
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
+  }
+}
+
+function getResults (query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
     .then(weather => {
       return weather.json();
     }).then(displayResults);
-
+}
 
 function displayResults (weather) {
   let city = document.querySelector('.location .city');
