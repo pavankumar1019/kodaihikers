0f6080e099e1373071c35d40aaaea51961dac378
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-19 23:04:48 +0530

    a

--- weather/main.js
+++ weather/main.js
@@ -2,6 +2,7 @@ const api = {
   key: "2b86e74fb5be73f5b02b0298b2b622fa",
   base: "https://api.openweathermap.org/data/2.5/"
 }
+let tempIcon = document.getElementById("temp-icon");
 window.addEventListener("load", () => {
   fetch(`${api.base}weather?q=kodaikanal&units=metric&APPID=${api.key}`)
     .then(weather => {
@@ -20,6 +21,28 @@ function displayResults (weather) {
 
   let weather_el = document.querySelector('.current .weather');
   weather_el.innerText = weather.weather[0].main;
+  const { id, icon, main } = data.weather[0];
+  if (id<250){
+    tempIcon.src = '../images/kodai_logo.png' ;
+  }
+  else if (id<350){
+    tempIcon.src = '../images/kodai_logo.png' ;
+  }
+  else if (id<550){
+    tempIcon.src = './icons/rain.svg' ;
+  }
+  else if (id<650){
+    tempIcon.src = './icons/snow.svg' ;
+  }
+  else if (id<800){
+    tempIcon.src = './icons/atmosphere.svg' ;
+  }
+  else if (id===800){
+    tempIcon.src = './icons/sun.svg' ;
+  }
+  else if(id>800){
+    tempIcon.src = '../images/kodai_logo.png' ;
+  }
 
   let hilow = document.querySelector('.hi-low');
   hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
