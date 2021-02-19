3967a603641987cd0ecd2cae37761752790d99c5
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-19 18:17:33 +0530

    a

--- about.php
+++ about.php
@@ -21,6 +21,12 @@ $result2 = $conn->query($sql2);
 <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
 <link rel="stylesheet" type="text/css" href="styles/about_styles.css">
 <link rel="stylesheet" type="text/css" href="styles/about_responsive.css">
+<style>
+p{
+	text-align: justify;
+
+}
+</style>
 </head>
 
 <body>
