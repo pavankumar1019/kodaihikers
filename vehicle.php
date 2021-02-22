8ac7933edde28a1576b8bbfd089c289f0a828153
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-22 21:42:29 +0530

    a

--- vehicle.php
+++ vehicle.php
@@ -352,7 +352,7 @@ $(document).ready(function(){
 });
 </script>
 <script>  
-
+$(document).ready(function(){
  
  fetch_data();
 
@@ -361,7 +361,6 @@ $(document).ready(function(){
   var action = "fetch";
   $.ajax({
    url:"fetch.php",
-   type:'POST',
    method:"POST",
    data:{action:action},
    success:function(data)
@@ -370,8 +369,79 @@ $(document).ready(function(){
    }
   })
  }
-
- 
+ $('#add').click(function(){
+  $('#imageModal').modal('show');
+  $('#image_form')[0].reset();
+  $('.modal-title').text("Add Image");
+  $('#image_id').val('');
+  $('#action').val('insert');
+  $('#insert').val("Insert");
+ });
+ $('#image_form').submit(function(event){
+  event.preventDefault();
+  var image_name = $('#image').val();
+  if(image_name == '')
+  {
+   alert("Please Select Image");
+   return false;
+  }
+  else
+  {
+   var extension = $('#image').val().split('.').pop().toLowerCase();
+   if(jQuery.inArray(extension, ['gif','png','jpg','jpeg']) == -1)
+   {
+    alert("Invalid Image File");
+    $('#image').val('');
+    return false;
+   }
+   else
+   {
+    $.ajax({
+     url:"action.php",
+     method:"POST",
+     data:new FormData(this),
+     contentType:false,
+     processData:false,
+     success:function(data)
+     {
+      alert(data);
+      fetch_data();
+      $('#image_form')[0].reset();
+      $('#imageModal').modal('hide');
+     }
+    });
+   }
+  }
+ });
+ $(document).on('click', '.update', function(){
+  $('#image_id').val($(this).attr("id"));
+  $('#action').val("update");
+  $('.modal-title').text("Update Image");
+  $('#insert').val("Update");
+  $('#imageModal').modal("show");
+ });
+ $(document).on('click', '.delete', function(){
+  var image_id = $(this).attr("id");
+  var action = "delete";
+  if(confirm("Are you sure you want to remove this image from database?"))
+  {
+   $.ajax({
+    url:"action.php",
+    method:"POST",
+    data:{image_id:image_id, action:action},
+    success:function(data)
+    {
+     alert(data);
+     fetch_data();
+    }
+   })
+  }
+  else
+  {
+   return false;
+  }
+ });
+});  
 </script>
 <script src="js/jquery-3.2.1.min.js"></script>
 <script src="styles/bootstrap4/popper.js"></script>
