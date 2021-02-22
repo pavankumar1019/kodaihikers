8830dd41bd8b7068c18994cf39fb2d0c409f7629
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-22 22:06:19 +0530

    a

--- fetch.php
+++ fetch.php
@@ -8,27 +8,25 @@ if(isset($_POST["action"]))
  {
   $query = "SELECT * FROM kh_add_hire_vehicles";
   $result = mysqli_query($conn, $query);
-  $output = '
-  <table class="table table-striped table-hover mt-5"   id="transposeThis"> 
-  <tr>
-           <td>'echo $row["pickup_place"].'</td>
-           <td>'.$row["drop_place"].'</td>
-           <td>"'.$row["vehicle_name"].'"</td>
-           <td>"'.$row["description"].'" </td>
-           <td>"'.$row["price"].'"</td>
-           <td>"'.$row["duration"].'" </td>
-           <td> "'.$row["distance_km"].'"</td>
-           <td> "'.$row["duration"].'"</td>
-           <td> "'.$row["distance_km"].'"</td>
-           <td>"'.$row["distance_km"].'"</td>
-           </tr>
- <tbody id="myTable" >
-  ';
+ 
   while($row = mysqli_fetch_array($result))
   {
-   $output .= '
-           
-   ';
+   $output .=?>
+   <table class="table table-striped table-hover mt-5"   id="transposeThis"> 
+   <tr>
+            <td><?php echo $row["pickup_place"];?></td>
+            <td><?php echo $row["pickup_place"];?></td>
+            <td><?php echo $row["pickup_place"];?></td>
+            <td><?php echo $row["pickup_place"];?> </td>
+            <td><?php echo $row["pickup_place"];?></td>
+            <td><?php echo $row["pickup_place"];?></td>
+            <td><?php echo $row["pickup_place"];?></td>
+            <td><?php echo $row["pickup_place"];?></td>
+            <td><?php echo $row["pickup_place"];?></td>
+            <td><?php echo $row["pickup_place"];?></td>
+            </tr>
+  <tbody id="myTable" >
+   <?php;
   }
   $output .= '</tbody></table>';
   echo $output;
