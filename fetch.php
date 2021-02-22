60c9e9f86498271f236e21ede50e9e6ddeb2002a
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-22 22:22:38 +0530

    a

--- fetch.php
+++ fetch.php
@@ -5,22 +5,20 @@ include 'db_connion/db.php';
 if(isset($_POST["action"]))
 {
 
- if($_POST["action"] == "fetch")
- {
- 
-  $output = '
-   <table class="table table-bordered table-striped">  
-    <tr>
-     <th width="10%">ID</th>
-     <th width="70%">Image</th>
-     <th width="70%">info</th>
-     <th width="10%">Change</th>
-     <th width="10%">Remove</th>
-    </tr>
-  ';
-  
-  $output .= '</table>';
-  echo $output;
- }
+                if($_POST["action"] == "fetch")
+                {
+                
+                $output = '
+                <table class="table table-bordered table-striped">  
+                    <tr>
+                    <th width="10%">ID</th>
+                    <th width="70%">Image</th>
+                    <th width="70%">info</th>
+                    <th width="10%">Change</th>
+                    <th width="10%">Remove</th>
+                    </tr>
+                ';
+                echo $output;
+                }
 }
  ?>
