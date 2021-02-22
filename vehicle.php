35301e593c847c0e46c2531f839e3475ddf9ba41
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-22 23:46:32 +0530

    a

--- vehicle.php
+++ vehicle.php
@@ -166,7 +166,7 @@ tr{
                               
                                                             ?>
 									
-                                    <form id="insert_form" method="post" enctype="multipart/form-data">
+                                    <form id="insert_form" action="#" method="post" enctype="multipart/form-data">
      <input type="hidden" name="id" value="<?php echo $row3["id"];?>" id="id" />
      <input type="submit" class="btn btn-primary btn-lg btn-block" id="insert" value="<?php echo $row3["pickup_place"]; ?>&nbsp;To&nbsp;<?php echo $row3["drop_place"]; ?>" class="btn btn-info" />
       
