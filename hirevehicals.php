195cd184e3ee36a974b15596be886f2ec11431d5
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-22 15:13:55 +0530

    a

--- hirevehicals.php
+++ hirevehicals.php
@@ -207,7 +207,7 @@ $result5 = $conn->query($sql5);
 									<div class="offers_content">
 										<div class="offers_price"><h2>Kodai Road</h2> <i class="fa fa-arrows-v" aria-hidden="true"></i> <h2>kodai Kanal</h2><span></span></div>
 										<div class="rating_r rating_r_4 offers_rating" data-rating="4">
-										<h4>Swift Dizer</h4>
+										<h4>Vehicle&nbsp; - &nbsp; Swift Dizer</h4>
 										</div>
 										<p class="offers_text">Suspendisse potenti. In faucibus massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu convallis tortor. Lorem ipsum dolor sit amet.</p>
 										<div class="offers_icons">
