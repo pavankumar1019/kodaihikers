90069bcbaf96a59b7b856cd21fcf4ab24b8b94d9
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-22 22:28:16 +0530

    a

--- fetch.php
+++ fetch.php
@@ -5,20 +5,41 @@ include 'db_connion/db.php';
 if(isset($_POST["action"]))
 {
 
-                if($_POST["action"] == "fetch")
-                {
-                
-                $output = '
-                <table class="table table-bordered table-striped">  
-                    <tr>
-                    <th width="10%">ID</th>
-                    <th width="70%">Image</th>
-                    <th width="70%">info</th>
-                    <th width="10%">Change</th>
-                    <th width="10%">Remove</th>
-                    </tr>
-                ';
-                echo $output;
-                }
+ if($_POST["action"] == "fetch")
+ {
+  $query = "SELECT * FROM kh_add_hire_vehicles where id=2";
+  $result = mysqli_query($conn, $query);
+  $output = '
+  <table class="table table-striped table-hover mt-5"   id="transposeThis">  
+    <tr>
+     <th>ID</th>
+     <th>Image</th>
+     <th>info</th>
+     <th>Change</th>
+     <th>Remove</th>
+    </tr>
+    <tbody id="myTable" >
+  ';
+  while($row = mysqli_fetch_array($result))
+  {
+   $output .= '
+
+    <tr>
+     <td>'.$row["id"].'</td>
+     <td>'.$row["id"].'</td>
+     <td>'.$row["id"].'</td>
+     <td>'.$row["id"].'</td>
+     <td>'.$row["id"].'</td>
+    
+    </tr>
+   ';
+  }
+  $output .= '
+  </tbody>
+  </table>';
+  echo $output;
+ }
 }
  ?>
+
+ 
