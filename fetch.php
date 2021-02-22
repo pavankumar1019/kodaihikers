9093e6a3836549282019a458b53cd011f673f8ea
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-22 22:01:23 +0530

    a

--- fetch.php
+++ fetch.php
@@ -10,40 +10,27 @@ if(isset($_POST["action"]))
   $result = mysqli_query($conn, $query);
   $output = '
   <table class="table table-striped table-hover mt-5"   id="transposeThis"> 
-   <tr>
-   <th>Pick Up Place</th>
-   <th>Drop Place</th> 
-   <th>Distance</th>
-         <th>Duration</th>
-   <th>Indigo</th>
-         <th>Swift Dzire</th>
-   <th>Etios</th>
-   <th>Tavera</th>
-         <th>Innova</th>
-   <th>Tempo Traveller</th>
- 
- </tr>
+  <tr>
+           <td> "'.$row["pickup_place"].'"</td>
+           <td> "'.$row["drop_place"].'"</td>
+           <td>"'.$row["vehicle_name"].'"</td>
+           <td>"'.$row["description"].'" </td>
+           <td>"'.$row["price"].'"</td>
+           <td>"'.$row["duration"].'" </td>
+           <td> "'.$row["distance_km"].'"</td>
+           <td> "'.$row["duration"].'"</td>
+           <td> "'.$row["distance_km"].'"</td>
+           <td>"'.$row["distance_km"].'"</td>
+           </tr>
  <tbody id="myTable" >
   ';
   while($row = mysqli_fetch_array($result))
   {
    $output .= '
-           <tr>
-           <td>jhgj</td>
-           <td>ghfhf</td>
-           <td>jhgjh</td>
-           <td>hjgjhg</td>
-           <td>hggj</td>
-           <td>hjgjg</td>
-           <td>jhghjg</td>
-           <td>jhgjg</td>
-           <td>hjggh</td>
-           <td>hghjg</td>
-           </tr>
+           
    ';
   }
-  $output .= '</tbody>
-  </table>';
+  $output .= '</tbody></table>';
   echo $output;
 }
  }
