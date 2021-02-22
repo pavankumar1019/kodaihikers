b433800ae7850c689decec5018034182d6ea34e6
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-22 14:44:37 +0530

    a

--- hirevehicals.php
+++ hirevehicals.php
@@ -525,7 +525,7 @@ $result5 = $conn->query($sql5);
 								</div>
 								<div class="col-lg-12">
 									<div class="offers_content">
-										<div class="offers_price">Kodai Road <i class="fa fa-exchange" aria-hidden="true"></i> kodai Kanal<span></span></div>
+										<div class="offers_price"><h6>Kodai Road</h6> <i class="fa fa-exchange" aria-hidden="true"></i> <h6>kodai Kanal</h6><span></span></div>
 										<div class="rating_r rating_r_4 offers_rating" data-rating="4">
 										
 										</div>
