0d617c144b7b550e8f953803fd1999675aa68de4
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-23 14:43:04 +0530

    a

--- bookvehicle_save.php
+++ bookvehicle_save.php
@@ -2,14 +2,14 @@
 	include 'db_connection/db.php';
 	$name=$_POST['name'];
 	$email=$_POST['email'];
-	$phone=$_POST['number'];
+	$phone=$_POST['phone'];
 	$noofperson=$_POST['noofperson'];
 	$pickupdate=$_POST['pickupdate'];
 	$dropdate=$_POST['dropdate'];
 	$pickupplace=$_POST['pickupplace'];
 	$dropplace=$_POST['dropplace'];
 	$message=$_POST['message'];
-	$sql = "INSERT INTO `kh_hire_vehicles`(`id`, `name`, `mobile_number`, `email`, `no_of_person`, `pickup_date`, `drop_date`, `pickup_place`, `drop_place`, `message`) 
+	$sql = "INSERT INTO `kh_hire_vehicles`(`name`,`email`, `mobile_number`, `no_of_person`, `pickup_date`, `drop_date`, `pickup_place`, `drop_place`, `message`) 
 	VALUES ('$name','$email','$phone','$noofperson','$pickupdate','$dropdate','$pickupplace','$dropplace','$message')";
 	if (mysqli_query($conn, $sql)) {
 		echo json_encode(array("statusCode"=>200));
