264f2851850d3f60427144119542b86c224862ea
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-02-23 14:07:30 +0530

    a

--- bookvehicle.php
+++ bookvehicle.php
@@ -180,9 +180,11 @@ $result5 = $conn->query($sql5);
 									placeholder="Phone Number" required="required" data-error="Subject is required.">
 								<input type="text" onkeypress="return onlyNumberKey(event)" id="noofperson" name="noofperson" class="contact_form_subject input_field"
 									placeholder="Number of person" required="required" data-error="Subject is required.">
+                                    <br>
                                    <p>Pickup date</p>
 								<input type="date"  id="pickupdate" name="pickupdate" class="contact_form_subject input_field"
 									placeholder="Pickup date" required="required" data-error="Subject is required.">
+                                    <br>
                                     <p>Drop date</p>
 								<input type="date"  id="dropdate" name="dropdate" class="contact_form_subject input_field"
 									placeholder="Drop date" required="required" data-error="Subject is required.">
