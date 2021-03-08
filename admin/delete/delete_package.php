8c98670d716b961bc40a7faaeb0c8957c4e4eb62
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-03-08 20:57:10 +0530

    Update delete_package.php

--- admin/delete/delete_package.php
+++ admin/delete/delete_package.php
@@ -2,7 +2,7 @@
 include '../../db_connection/db.php';
 $id = $_POST['idelete'];
 $query = "delete from kh_book_my_tours  where id = '".$id."'";  //query 
-if(mysql_query($conn, $query)){ //check is true or false 
+if(mysqli_query($conn, $query)){ //check is true or false 
  echo "deleted id ".$id." ";//out put if ture
  }
  else{ 
