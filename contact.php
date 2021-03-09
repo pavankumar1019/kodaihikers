4e86fe0cfcf056a1853f26e7083f69ea8ae2fa4a
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-03-09 14:00:40 +0530

    a

--- contact.php
+++ contact.php
@@ -16,7 +16,7 @@ $result5 = $conn->query($sql5);
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Contact</title>
+<title>Contact kodai hikers tours and travels</title>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="Travelix Project">
