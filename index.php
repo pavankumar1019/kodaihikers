6f29ddfe24c7449b6d373c7953d71257ef8689c6
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-03-09 13:59:55 +0530

    a

--- index.php
+++ index.php
@@ -22,10 +22,10 @@ $result5 = $conn->query($sql5);
 <html lang="en">
 
 <head>
-	<title>KODI HIKERS</title>
+	<title>Kodai Hikers (Tours and Travels)</title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="description" content="Kodai Hikers is a best tours and travels agency in Kodaikanal ">
+	<meta name="description" content="Kodai Hikers is a best tours and travels agency in Kodaikanal, budget tour packages,best hotels services, best vehicle services">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
 	<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
